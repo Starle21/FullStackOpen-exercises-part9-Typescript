ce248587f9e3f3cbed51f54d47b22d1d75a41d99
Starle21 <staronova.lenka@gmail.com>
2021-08-12 15:24:50 +0200

    Part 9.10 Patientor backend - diagnoses endpoint

--- patientor_backend/src/index.ts
+++ patientor_backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import diagnoseRouter from "./routes/diagnoses";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("patientor pong");
 });
+
+app.use("/api/diagnoses", diagnoseRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
