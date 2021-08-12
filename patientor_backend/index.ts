473653d379a316811161a21444fe91852fb0c821
Starle21 <staronova.lenka@gmail.com>
2021-08-12 11:35:09 +0200

    Part 9.9 Patientor - connecting frontend

--- patientor_backend/index.ts
+++ patientor_backend/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("patientor pong");
 });
